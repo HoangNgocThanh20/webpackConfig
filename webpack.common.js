3f2651d712347ba08fd48299607b134d10d3b94d
thanhhn <ngocthanhdev170120@gmail.com>
2024-04-22 22:26:06 +0700

    add split webpack and html-loader and asset management config clean webpack

---
+++ webpack.common.js
@@ -0,0 +1,23 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+  module: {
+    rules: [
+      { test: /\.scss$/, use: ["style-loader", "css-loader", "sass-loader"] },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
